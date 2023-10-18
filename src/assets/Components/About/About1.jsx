8a684c7182ffb1d49113de1f1151c35d7e23a20a
justinait <justiiturraspe@gmail.com>
2023-10-18 18:41:15 -0300

    detalles cele x2

--- src/assets/Components/About/About1.jsx
+++ src/assets/Components/About/About1.jsx
@@ -10,8 +10,12 @@ function About1() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="15" viewBox="0 0 15 15" fill="none" x="3%">                    <path d="M5.81967 15C6.40984 15 6.91803 14.7164 7.2623 14.1657L14.5902 2.50278C14.8033 2.15239 15 1.75195 15 1.38487C15 0.550612 14.2787 0 13.5082 0C13.0164 0 12.5738 0.283648 12.2295 0.83426L5.7541 11.4127L2.70492 7.45829C2.31148 6.94105 1.91803 6.75751 1.42623 6.75751C0.622951 6.75751 0 7.42492 0 8.24249C0 8.64294 0.147541 9.01001 0.42623 9.3604L4.29508 14.1824C4.7377 14.7497 5.21311 15 5.81967 15Z" fill="#7E7E80"/>                </svg>
                 Experiencia
             </h4>
-            <p className='aboutCardText'>Fruto de más de veinte años de solidez y trayectoria que te permitirán 
-                concretar tus objetivos minimizando riesgos y evitando pérdidas innecesarias.
+            <p className='aboutCardText'>
+            Fruto de más de veinte años de<br/>
+            solidez y trayectoria que te <br/>
+            permitirán concretar tus <br/>
+            objetivos minimizando riesgos y <br/>
+            evitando pérdidas innecesarias.
             </p>
         </div>
         
@@ -21,8 +25,13 @@ function About1() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="15" viewBox="0 0 15 15" fill="none" x="3%">                    <path d="M5.81967 15C6.40984 15 6.91803 14.7164 7.2623 14.1657L14.5902 2.50278C14.8033 2.15239 15 1.75195 15 1.38487C15 0.550612 14.2787 0 13.5082 0C13.0164 0 12.5738 0.283648 12.2295 0.83426L5.7541 11.4127L2.70492 7.45829C2.31148 6.94105 1.91803 6.75751 1.42623 6.75751C0.622951 6.75751 0 7.42492 0 8.24249C0 8.64294 0.147541 9.01001 0.42623 9.3604L4.29508 14.1824C4.7377 14.7497 5.21311 15 5.81967 15Z" fill="#7E7E80"/>                </svg>
                 Profesionalidad
             </h4>
-            <p className='aboutCardText'>Basada en la calidad de las personas que conforman nuestro  equipo, 
-                nuestra organización se enorgullece de contar con un grupo excepcional de profesionales.</p>
+            <p className='aboutCardText'>
+            Basada en la calidad de las <br/>
+            personas que conforman <br/>
+            nuestro equipo, nuestra <br/>
+            organización se enorgullece <br/>
+            de contar con un grupo <br/>
+            excepcional de profesionales.</p>
         </div>
         
         
@@ -31,8 +40,13 @@ function About1() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="15" viewBox="0 0 15 15" fill="none" x="3%">                    <path d="M5.81967 15C6.40984 15 6.91803 14.7164 7.2623 14.1657L14.5902 2.50278C14.8033 2.15239 15 1.75195 15 1.38487C15 0.550612 14.2787 0 13.5082 0C13.0164 0 12.5738 0.283648 12.2295 0.83426L5.7541 11.4127L2.70492 7.45829C2.31148 6.94105 1.91803 6.75751 1.42623 6.75751C0.622951 6.75751 0 7.42492 0 8.24249C0 8.64294 0.147541 9.01001 0.42623 9.3604L4.29508 14.1824C4.7377 14.7497 5.21311 15 5.81967 15Z" fill="#7E7E80"/>                </svg>
                 Personalización
             </h4>
-            <p className='aboutCardText'>Nos destacamos por nuestra capacidad para identificar las necesidades de cada cliente y 
-                ofrecer soluciones específicas que se ajusten perfectamente a sus requisitos individuales.</p>
+            <p className='aboutCardText'>
+                Nos destacamos por nuestra <br/>
+                capacidad para identificar las <br/>
+                necesidades de cada cliente y <br/>
+                ofrecer soluciones específicas <br/>
+                que se ajusten perfectamente a <br/>
+                sus requisitos individuales.</p>
         </div>
         
     </div>

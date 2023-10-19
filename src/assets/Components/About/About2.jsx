4650ab5ee7f3cb9b068c85c8f5cb428efb7dcb20
justinait <justiiturraspe@gmail.com>
2023-10-19 16:57:49 -0300

    accordion about us

--- src/assets/Components/About/About2.jsx
+++ src/assets/Components/About/About2.jsx
@@ -11,30 +11,43 @@ function About2() {
 
         <Accordion>
             <Accordion.Item eventKey="0">
-                <Accordion.Header> <h4 className='accordionTitle'>+ Misión</h4> </Accordion.Header>
+                <Accordion.Header> <h4 className='iconPlus'>+</h4> <h4 className='accordionTitle'>Misión</h4> </Accordion.Header>
                 <Accordion.Body>
                     <p className='accordionText'>
-                        Brindar tranquilidad y seguridad financiera a nuestros clientes actuales y potenciales, 
-                        guiándolos hacia las opciones que mejor se adapten a sus necesidades en ámbitos de seguros, 
-                        finanzas y salud; y ofreciendo un servicio insuperable de asesoramiento y gestión de cobros y reclamos.
+                        Brindar <strong>tranquilidad y seguridad</strong> <br/>
+                        financiera a nuestros clientes actuales <br/>
+                        y potenciales, guiándolos hacia las <br/>
+                        opciones que <strong> mejor se adapten a sus <br/>
+                        necesidades</strong> en ámbitos de seguros, <br/>
+                        finanzas y salud; y ofreciendo un <br/>
+                        servicio insuperable de <strong> asesoramiento <br/>
+                        y gestión de cobros y reclamos.</strong>
                     </p>
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
-                <Accordion.Header> <h4 className='accordionTitle'>+ Visión</h4> </Accordion.Header>
+                <Accordion.Header> <h4 className='iconPlus'>+</h4><h4 className='accordionTitle'>Visión</h4> </Accordion.Header>
                 <Accordion.Body>
                 <p className='accordionText'>
-                    Aspiramos a ser el líder regional, tanto para empresas como para personas, 
-                    a través del compromiso de nuestro equipo en la entrega del mejor servicio posible.
+                    Aspiramos a ser el <strong> líder regional</strong>, <br/>
+                    tanto para empresas como para  <br/>
+                    personas, a través del compromiso  <br/>
+                    de nuestro equipo en la entrega del  <br/>
+                    <strong>mejor servicio posible.</strong>
                 </p>
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="2">
-                <Accordion.Header> <h4 className='accordionTitle'>+ Valores</h4> </Accordion.Header>
+                <Accordion.Header> <h4 className='iconPlus'>+</h4> <h4 className='accordionTitle'>Valores</h4> </Accordion.Header>
                 <Accordion.Body>
                 <p className='accordionText'>
-                    Los valores en los que se sustenta el trabajo que se lleva a cabo día a día dentro de la organización, 
-                    son los siguientes: compromiso, responsabilidad, profesionalismo, empatía con clientes y proveedores y excelencia.
+                    Los valores en los que se sustenta <br/>
+                    el trabajo que se lleva a cabo día a  <br/>
+                    día dentro de la organización, son  <br/>
+                    los siguientes: <strong> compromiso, <br/>
+                    responsabilidad, profesionalismo,  <br/>
+                    empatía </strong> con clientes y  <br/>
+                    proveedores y excelencia.
                 </p>
                 </Accordion.Body>
             </Accordion.Item>

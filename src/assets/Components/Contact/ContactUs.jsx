718e5908fc55aa8889ee7b38df7363769091a4bd
justinait <justiiturraspe@gmail.com>
2023-10-23 17:22:41 -0300

    desktop responsive

--- src/assets/Components/Contact/ContactUs.jsx
+++ src/assets/Components/Contact/ContactUs.jsx
@@ -4,11 +4,14 @@ import './ContactUs.css'
 function ContactUs() {
   return (
     <div className='contactContainer'>
-      <h3>CONTACTANOS</h3>
-      <h4>Despejá todas tus dudas, cotizaciones, vigencias, <br />packs de productos, o cualquier consulta. </h4>
-      <h4 className='subtitleContact'>Estamos para asegurarte</h4>
-      
-      <h4 className='contactText'>Completá el siguiente formulario y un ejecutivo <br /> se comunicará contigo a la brevedad</h4>
+      <div className='contactOrangeContainer'>
+        <h3>CONTACTANOS</h3>
+        <h4>Despejá todas tus dudas, cotizaciones, vigencias, <br />packs de productos, o cualquier consulta. </h4>
+        <h4 className='subtitleContact'>Estamos para asegurarte</h4>
+        
+        <h4 className='contactText'>Completá el siguiente formulario y un ejecutivo <br /> se comunicará contigo a la brevedad</h4>
+
+      </div>
     </div>
   )
 }

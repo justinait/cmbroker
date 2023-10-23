1e0ff8d6c86f4693881034d27c896c5e6d884e1e
justinait <justiiturraspe@gmail.com>
2023-10-23 12:18:59 -0300

    tablet and desktop, home and about

--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 import photo1 from '../../images/happycoupleHERO.jpg'
 import photo2 from '../../images/workHERO.png'
 import photo3 from '../../images/team.jpg'
 import photo4 from '../../images/office.jpg'
+import photo5 from '../../images/familycar.jpg'
+import photo6 from '../../images/work1.png'
+
 import './Home.css'
 
 function Home() {
   
-  const dataBranding = [
-    { text2: 'Para vos', image: photo1},
-    { text2: 'Para tu profesión', image: photo2},
-    { text2: 'Para tu negocio', image: photo3},
-    { text2: 'Para empresas', image: photo4},
-  ];
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const actualizarTamañoPantalla = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', actualizarTamañoPantalla);
+
+    return () => {
+      window.removeEventListener('resize', actualizarTamañoPantalla);
+    };
+  }, []);
+  let dataBranding = [];
+  if (width <= 768) {
+    dataBranding = [
+      { text2: 'Para vos', image: photo1},
+      { text2: 'Para tu profesión', image: photo2},
+      { text2: 'Para tu negocio', image: photo3},
+      { text2: 'Para empresas', image: photo4},
+    ];
+  } else {
+    dataBranding = [
+      { text2: 'Para vos', image: photo5, className: 'imagesRight'},
+      { text2: 'Para tu profesión', image: photo6, className: 'imagesRight'},
+      { text2: 'Para tu negocio', image: photo3},
+      { text2: 'Para empresas', image: photo4},
+    ];
+  }
 
   return (
     <Carousel id='home'>
@@ -25,7 +51,7 @@ function Home() {
                 <h4 className='carouselText1'>SERVICIOS + SEGUROS</h4>
                 <h4 className='carouselText2'>{e.text2}</h4>
               </div>
-              <img src={e.image} alt="" className='carouselImageMobile'/>
+              <img src={e.image} alt="" className={`carouselImageMobile ${e.className}`}/>
             </Carousel.Item>      
 
           )

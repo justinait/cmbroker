6a27826c1ecb6bdadeba3ff9deee1e7b13ed6704
justinait <justiiturraspe@gmail.com>
2023-10-26 18:32:50 -0300

    config contact form email js

--- src/assets/Components/Contact/ContactForm.jsx
+++ src/assets/Components/Contact/ContactForm.jsx
@@ -1,30 +1,37 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import './ContactForm.css'
 
 export const ContactForm = () => {
   const form = useRef();
 
+  const {service_id, template_id, public_id} = config.EMAIL;
+
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
+    emailjs.sendForm(service_id, template_id, form.current, public_id)
+    .then((result) => {
+      console.log(result.text);
+      // aca va success modal
+    }, (error) => {
+      console.log(error.text);
+    });
   };
 
   return (
     <form ref={form} onSubmit={sendEmail}>
       
-      <input type="text" name="user_name" placeholder='Nombre y apellido'/>
+      <input type="text" name="user_name" placeholder='Nombre y apellido' className='inputForm'/>
       
-      <input type="email" name="user_email" placeholder='Email'/>
+      <input type="email" name="user_email" placeholder='Email' className='inputForm'/>
+
+      <input type="email" name="user_phone" placeholder='Teléfono / Celular' className='inputForm'/>
       
-      <textarea name="message" placeholder='Mensaje' />
+      <textarea name="message" placeholder='Mensaje'  className='inputForm'/>
       
       <input type="submit" value="Enviar" />
+
     </form>
   );
 };

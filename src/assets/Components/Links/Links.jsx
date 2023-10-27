040eb32028d17131dff187154577e089a4759ae1
justinait <justiiturraspe@gmail.com>
2023-10-27 12:09:11 -0300

    link hovers

--- src/assets/Components/Links/Links.jsx
+++ src/assets/Components/Links/Links.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import wsp from '../../images/icons/wsp.svg'
 import mail from '../../images/icons/mail.svg'
+import wsphover from '../../images/icons/wsphover.svg'
+import mailhover from '../../images/icons/mailhover.svg'
 import './Links.css'
 
 function Links() {
@@ -9,11 +11,13 @@ function Links() {
         <a target="_blank" href="https://wa.link/d987ps">
             <div className='linkIconDiv'>    
                 <img src={wsp} alt="Whatsapp" className='linkIcons'/>
+                <img src={wsphover} alt="Whatsapp" className='linkIconsHover'/>
             </div>
         </a>
         <a href="mailto:comercial@cmbroker.com.ar">
             <div className='linkIconDiv'>
                 <img src={mail} alt="Correo" className='linkIcons'/>
+                <img src={mailhover} alt="Correo" className='linkIconsHover'/>
             </div>
         </a>
     </div>

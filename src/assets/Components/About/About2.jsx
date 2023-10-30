bf8f3ab7c206743a47d82ee5b3912a30ce52e549
justinait <justiiturraspe@gmail.com>
2023-10-30 12:08:01 -0300

    responsive acoordion

--- src/assets/Components/About/About2.jsx
+++ src/assets/Components/About/About2.jsx
@@ -15,13 +15,18 @@ function About2() {
                 <Accordion.Body>
                     <p className='accordionText'>
                         Brindar <strong>tranquilidad y seguridad</strong> <br className='mobileBreak'/>
-                        financiera a nuestros clientes actuales <br className='mobileBreak'/>
+                        financiera a <br className='tabletBreak'/>
+                        nuestros clientes actuales <br className='mobileBreak'/>
                         y <br className='desktopBreak'/>
-                         potenciales, guiándolos hacia las <br className='mobileBreak'/>
-                        opciones que <strong> mejor se adapten a sus <br className='mobileBreak'/>
+                         potenciales, guiándolos <br className='tabletBreak'/>
+                         hacia las <br className='mobileBreak'/>
+                        opciones que <strong> mejor se adapten a sus <br/>
                         necesidades</strong> en ámbitos de seguros, <br className='mobileBreak'/>
-                        finanzas y salud; y ofreciendo un <br className='mobileBreak'/>
-                        servicio insuperable de <strong> asesoramiento <br className='mobileBreak'/>
+                        finanzas y <br className='tabletBreak'/>
+                        salud; y ofreciendo un <br className='mobileBreak'/>
+                        servicio <br className='desktopBreak'/>
+                         insuperable de <br className='tabletBreak'/>
+                        <strong> asesoramiento <br className='mobileBreak'/>
                         y gestión de cobros y reclamos.</strong>
                     </p>
                 </Accordion.Body>
@@ -31,9 +36,13 @@ function About2() {
                 <Accordion.Body>
                 <p className='accordionText'>
                     Aspiramos al <strong> liderazgo regional</strong>, <br className='mobileBreak'/>
-                    en los segmentos empresas y  <br className='mobileBreak'/>
-                    personas, a través del compromiso  <br className='mobileBreak'/>
-                    de nuestro equipo en la entrega del  <br className='mobileBreak'/>
+                    en los  <br className='tabletBreak'/>
+                    segmentos empresas y  <br className='mobileBreak'/>
+                    personas, a  <br className='desktopBreak'/>
+                    través del  <br className='tabletBreak'/>
+                    compromiso  <br className='mobileBreak'/>
+                    de nuestro equipo en la entrega <br className='tabletBreak'/>
+                     del  <br className='mobileBreak'/>
                     <strong>mejor servicio posible.</strong>
                 </p>
                 </Accordion.Body>
@@ -43,10 +52,9 @@ function About2() {
                 <Accordion.Body>
                 <p className='accordionText'>
                     Nuestro trabajo se sustenta con <br className='mobileBreak'/>
-                    {/* el trabajo que se lleva a cabo día a  <br className='mobileBreak'/>
-                    día dentro de la organización, son  <br className='mobileBreak'/> */}
-                    <strong> compromiso, responsabilidad,  <br className='mobileBreak'/>
-                    profesionalismo, empatía </strong> con  <br className='mobileBreak'/>
+                    <strong> compromiso,  <br className='tabletBreak'/>
+                        responsabilidad,  <br className='mobileBreak desktopBreak'/>
+                    profesionalismo, empatía </strong> con  <br className='tabletBreak mobileBreak break'/>
                     clientes y proveedores y excelencia.
                 </p>
                 </Accordion.Body>

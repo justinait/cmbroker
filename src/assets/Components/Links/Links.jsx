fb87cb03dad621e8a6690e55e0fe29a1f60df7be
justinait <justiiturraspe@gmail.com>
2023-10-31 12:06:21 -0300

    redes

--- src/assets/Components/Links/Links.jsx
+++ src/assets/Components/Links/Links.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import wsp from '../../images/icons/wsp.svg'
 import mail from '../../images/icons/mail.svg'
 import wsphover from '../../images/icons/wsphover.svg'
@@ -6,8 +6,33 @@ import mailhover from '../../images/icons/mailhover.svg'
 import './Links.css'
 
 function Links() {
+    const [isFixed, setIsFixed] = useState(true);
+  
+    useEffect(() => {
+        const footerHeight = 170; // Altura de tu footer
+        const handleScroll = () => {
+          if (window.scrollY + window.innerHeight >= document.body.scrollHeight - footerHeight) {
+            // Detén los íconos antes de que lleguen al footer
+            setIsFixed(false);
+          } else {
+            setIsFixed(true);
+          }
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    
+    const socialIconsStyle = {
+        position: isFixed ? 'fixed' : 'absolute',
+        bottom: isFixed ? '60px' : 'auto',
+        right: isFixed ? '17px' : 'auto',
+    };
   return (
-    <div className='links'>
+    <div className='links' style={socialIconsStyle}>
         <a target="_blank" href="https://wa.link/d987ps">
             <div className='linkIconDiv'>    
                 <img src={wsp} alt="Whatsapp" className='linkIcons'/>

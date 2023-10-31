b95330a600d14ae2b7ae7f54f90e6b3feb223a77
justinait <justiiturraspe@gmail.com>
2023-10-30 21:01:25 -0300

    alert

--- src/assets/Components/Contact/ContactForm.jsx
+++ src/assets/Components/Contact/ContactForm.jsx
@@ -1,20 +1,114 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import './ContactForm.css'
 import config from '../../../config/config';
+import Modal from 'react-bootstrap/Modal';
 
 export const ContactForm = () => {
-  const form = useRef();
 
+  const form = useRef();
   const {service_id, template_id, public_id} = config.EMAIL;
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const [btnsubmit, setBtnsubmit] = useState(true);
+  
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [input, setInput] = useState({
+    user_name:{
+      value:'',
+      error:''
+    },
+    user_email:{
+      value:'',
+      error:''
+    },
+    user_phone:{
+      value:'',
+      error:''
+    },
+    message:{
+      value:'',
+      error:''
+    }
+  });
+
+  const estilos ={
+    color:"red",
+    paddingLeft:"25px",
+    margin:"0px",
+  }
+
+  const handleInputChange = (e) =>{
+    setInput(prev=>({
+      ...prev,
+      [e.target.name]:{
+        value:e.target.value,
+        error:null
+      }
+    }));    
+  };
+
+  const clearInputValue = () =>{
+    setInput({
+      user_name:{
+        value:'',
+        error:''
+      },
+      user_email:{
+        value:'',
+        error:''
+      },
+      user_phone:{
+        value:'',
+        error:''
+      },
+      message:{
+        value:'',
+        error:''
+      }
+    });    
+  };
+
+  useEffect(() => {
+    if (show) {
+      const timer = setTimeout(() => {
+        setShow(false);
+      }, 1500);
+
+      return () => clearTimeout(timer);
+    }
+  }, [show]);
+
+  const sendEmailForm = (e) => {
+    e.preventDefault();
+    let stop = false;
+    Object.keys(input).forEach(key=>{
+      if (input[key].value.trim().length === 0) {
+        stop = true;
+        setInput(prev=>({
+          ...prev,
+          [key]:{
+            ...prev[key],
+            error:'*Este campo es obligatorio'
+          }
+        }));
+      }
+    });
+    if(stop) return;
+
+    setBtnsubmit(true);
+    
+    handleShow()
+    
     emailjs.sendForm(service_id, template_id, form.current, public_id)
     .then((result) => {
       console.log(result.text);
-      // aca va success modal
+      
+      clearInputValue();
+      setBtnsubmit(false);
     }, (error) => {
       console.log(error.text);
     });
@@ -22,21 +116,32 @@ export const ContactForm = () => {
 
   return (
     <div>
-      <form ref={form} onSubmit={sendEmail} className='formContainer'>
+      <form ref={form} onSubmit={sendEmailForm} className='formContainer'>
         
-        <input type="text" name="user_name" placeholder='Nombre y apellido' className='inputForm'/>
+        <input type="text" name="user_name" placeholder='Nombre y apellido'  value={input['user_name'] ? input['user_name'].value : ''}  className='inputForm' onChange={handleInputChange} />
         
-        <input type="email" name="user_email" placeholder='Email' className='inputForm'/>
+        <input type="email" name="user_email" placeholder='Email' value={input['user_email'] ? input['user_email'].value : ''} className='inputForm' onChange={handleInputChange} />
 
-        <input type="number" name="user_phone" placeholder='Teléfono / Celular' className='inputForm'/>
+        <input type="number" name="user_phone" placeholder='Teléfono / Celular' value={input['user_phone'] ? input['user_phone'].value : ''} className='inputForm' onChange={handleInputChange} />
         
-        <textarea name="message" placeholder='Mensaje'  className='inputForm messageForm'/>
+        <textarea name="message" placeholder='Mensaje' value={input['message'] ? input['message'].value : ''} className='inputForm messageForm'  onChange={handleInputChange}/>
         
-        <div >
+        <div>
           <input type="submit" value="Enviar" className='formSendButton'/>
         </div>
 
       </form>
+
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton />
+        <Modal.Body>
+          <p>¡Gracias por tu mensaje!</p>
+          <p className='modalText'>Tu formulario se ha enviado con éxito. <br/>
+            Nos pondremos en contacto contigo en breve.</p>
+        </Modal.Body>
+        
+      </Modal>
     </div>
   );
 };
